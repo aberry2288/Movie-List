a8d7666e9d60eac8777e0d1bb76f5a6107ea8f3d
Adam Berry <aberry2288@gmail.com>
2023-07-26 17:25:03 -0400

    updated js, get movies button working

--- js/site.js
+++ js/site.js
@@ -1,13 +1,44 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjZkZGM2MGNkMmY4YmU5OWNmMTY4YjI2ZGY0YmNjMSIsInN1YiI6IjY0YzE2ODI4MWNmZTNhMGViNDI5NDhiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQnztwiv1mKrqLiF4y2FytksVOG8365dJjo2N_3zFCA'
 
-    let message = inputBox.value;
+//https://api.themoviedb.org/3/movie/popular
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+async function getMovies() {
+
+    try {
+
+        let response = await fetch('https://api.themoviedb.org/3/movie/popular', {
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        let data = await response.json();
+
+        return data;
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
 }
+
+async function displayMovies() {
+
+    const movieListDiv = document.getElementById('movie-list');
+
+    let data = await getMovies();
+
+    let movies = data.results; //movies is an array of objects
+
+    movies.forEach(movie => {        
+
+        let paragraphTag = document.createElement('p');
+
+        paragraphTag.innerText = movie.title;
+
+        movieListDiv.appendChild(paragraphTag);
+    });
+
+}
+
